0f0e976aebbfd110d3edda2a0b7fa974b741a079
Mick Evans <codemastermick@gmail.com>
2019-08-11 09:56:06 -0400

    feat: string the echo onto the end of push
    Makes for better feedback

--- .travis/push.sh
+++ .travis/push.sh
@@ -3,5 +3,4 @@
 git remote rm origin
 git remote add origin https://codemastermick:${GH_TOKEN}@github.com/codemastermick/ng-devops.git >/dev/null 2>&1
 git pull
-git push origin master --quiet
-echo "Changes pushed to github"
+git push origin master --quiet && echo "Changes pushed to github"
