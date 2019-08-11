f58be320db95fcc3bdf9db70bfb620c8321dd8d3
Mick Evans <codemastermick@gmail.com>
2019-08-11 10:06:05 -0400

    chore: add validation to feedback

--- .travis/push.sh
+++ .travis/push.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 
+success=1
+
 git remote rm origin
 git remote add origin https://codemastermick:${GH_TOKEN}@github.com/codemastermick/ng-devops.git >/dev/null 2>&1
 git pull
-git push origin master --quiet && echo "Changes pushed to github"
+git push origin master --quiet && success=0
+
+if [[ $success == 0 ]]; then
+  echo "Changes pushed to Github"
+else
+  echo "Failed to push changes to Github"
+fi
+
+exit $success
