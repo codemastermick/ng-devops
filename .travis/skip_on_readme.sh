856c09872f9d1c6da3fe835fb3708b0cfa4050db
Mick Evans <51538065+codemastermick@users.noreply.github.com>
2019-08-11 09:26:49 -0400

    chore: fix issues from Codacy (#8)

--- .travis/skip_on_readme.sh
+++ .travis/skip_on_readme.sh
@@ -2,8 +2,6 @@
 
 if ! git diff --name-only $TRAVIS_COMMIT_RANGE | grep -qvE '(.md$)'
 then
-currentCommitMessage=`echo "$(cat $1)" | head -1`
-
   echo "Only docs were updated, not running the CI."
   exit
 fi
