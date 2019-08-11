1b06dd1e233c7a6b07edd77c7bacdcac7199ed72
Mick Evans <codemastermick@gmail.com>
2019-08-11 09:34:42 -0400

    feat: add preliminary support for github pushing

---
+++ .travis/github_login.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+git config --global user.email "travis@travis-ci.org" # set email
+git config --global user.name "Travis CI" # set username
+
+username=echo "$(git config --global user.name)"
+email=echo "$(git config --global user.email)"
+
+git checkout master # checkout master branch
+git pull # pull any changes
+
+echo "Logged into git as $username with $email"
