d70a45eb128e4d9f0fc867a946df058a3cee42a2
Mick Evans <codemastermick@gmail.com>
2019-08-11 09:55:11 -0400

    fix: wrong repo in add origin

--- .travis/push.sh
+++ .travis/push.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 git remote rm origin
-git remote add origin https://codemastermick:${GH_TOKEN}@github.com/codemastermick/profile-page.git >/dev/null 2>&1
+git remote add origin https://codemastermick:${GH_TOKEN}@github.com/codemastermick/ng-devops.git >/dev/null 2>&1
 git pull
 git push origin master --quiet
 echo "Changes pushed to github"
